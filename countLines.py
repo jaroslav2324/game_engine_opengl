f6bdc0cd57f6c7bff4d41dfa4ef58f1a61b0c23a
jaroslav2324 <jaro_r@mail.ru>
2024-05-02 08:27:03 +0300

    Modified countLines script

--- countLines.py
+++ countLines.py
@@ -1,5 +1,14 @@
 import os
 
+def count_lines_by_semicolon(directory):
+    line_count = 0
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(('.cpp', '.h')):
+                with open(os.path.join(root, file), 'r') as f:
+                    line_count += sum(1 for line in f if ';' in line)
+    return line_count
+
 def count_lines(directory):
     line_count = 0
     for root, dirs, files in os.walk(directory):
@@ -9,4 +18,5 @@ def count_lines(directory):
                     line_count += sum(1 for line in f)
     return line_count
 
-print(count_lines('src'))
+print("Count lines (\\n)", count_lines('src'))
+print("Count lines ( ;)", count_lines_by_semicolon('src'))
